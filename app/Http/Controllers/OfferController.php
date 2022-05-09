f4900b54ff7f65bde9ff56d84aa6e5756241a0f7
Dmitrij Kunts <r206265@gmail.com>
2022-05-09 22:37:51 +0300

    refactoring

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -25,21 +25,25 @@ class OfferController extends Controller
         $res = collect(Offer::where('merchant', $merchant)->get())->map(function ($i) {
             return "{$i->code}\t" . getULP($i->url);
         })->implode(PHP_EOL) . PHP_EOL;
-        return response($res, 200, ['Content-Type' => 'text/html; charset=UTF-8']);
+        return $this->responseTextHtml($res);
     }
 
     public function presentsDescription(string $code)
     {
         $o = Offer::where('code', $code)->firstOrFail();
 
-        return response($o->description, 200, ['Content-Type' => 'text/html; charset=UTF-8']);
+        return $this->responseTextHtml($o->description);
     }
 
     public function presentsName(string $code)
     {
         $o = Offer::where('code', $code)->firstOrFail();
 
-        return response($o->name, 200, ['Content-Type' => 'text/html; charset=UTF-8']);
+        return $this->responseTextHtml($o->name);
     }
 
+    private function responseTextHtml($content)
+    {
+        return response($content, 200, ['Content-Type' => 'text/html; charset=UTF-8']);
+    }
 }
